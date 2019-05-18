14e1488b62586b37832666c8fd64cc7f8cc8cd52
wmc7n <44489337+wmc7n@users.noreply.github.com>
2019-05-18 22:37:40 +0900

    Update bit52.lua

--- bit52/bit52.lua
+++ bit52/bit52.lua
@@ -36,22 +36,16 @@ M.xor52 = function(v1, v2)
 	return h*hi + l
 end
 
-M.not52 = function(v1)
-	local hi = 0x80000000
-	local low = 0x7fffffff
-	local hi1 = math.floor(v1 / hi)
-	local low1 = bit.band(v1, low)
-	local h = bit.bnot(hi1)
-	local l = bit.bnot(low1)
-	return h*hi + l
-end
-
 M.ls52 = function(num, bits)
 	return num * math.pow(2, bits)
 end
 
 M.rs52 = function(num, bits)
 	return math.floor(num / math.pow(2, bits))
+end
+
+M.not52 = function(v1)
+	return bit.bnot(bit.band(v1, 0xffffffff)) + M.ls52(bit.band(bit.bnot(M.rs52(v1, 32)), 0x7FFFF), 32)
 end
 
 M.to_num = function(bits)
